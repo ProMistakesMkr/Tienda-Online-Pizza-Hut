3574d6f5ad8535741815cda7c64925226875fe58
Nicolás Sánchez <nicksansa12@gmail.com>
2021-05-25 19:16:18 -0500

    Código prototipo

---
+++ Pizza-Hut/src/Principal.java
@@ -0,0 +1,29 @@
+import processing.core.PApplet;
+
+public class Principal extends PApplet
+{
+
+	public static void main(String[] args) 
+	{
+		PApplet.main("Principal");
+	}
+	
+	@Override
+	public void settings() //void Awake
+	{
+		size(500, 500);
+	}
+	
+	@Override
+	public void setup() //void Start
+	{
+		
+	}
+	
+	@Override
+	public void draw() //void Update
+	{		
+		
+	}
+
+}

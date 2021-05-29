28ac7080305b0e73254cffea8a5a6725e2a6a70e
Nicolás Sánchez <nicksansa12@gmail.com>
2021-05-29 09:06:00 -0500

    Clases view

---
+++ Pizza-Hut/src/Principal.java
@@ -0,0 +1,55 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+import view.FirstScreen;
+import view.RegisterScreen;
+import view.CatalogScreen;
+import view.PaymentScreen;
+import view.GoodScreen;
+import view.SummaryScreen;
+import view.OutScreen;
+public class Principal extends PApplet {
+
+	PImage img;
+
+	public static void main(String[] args) {
+		PApplet.main("Principal");
+	}
+	FirstScreen firstscreen;
+	RegisterScreen registerscreen;
+	CatalogScreen catalogscreen;
+	PaymentScreen paymentscreen;
+	GoodScreen goodscreen;
+	SummaryScreen summaryscreen;
+	OutScreen outscreen;
+	
+	public boolean error = false;
+	public int loginYes=0;
+	public int loginMay=0;
+	public int schange=0;
+	public int ticketOne=0;
+	public int ticket1=0;
+	public int ticketTwo=0;
+	public int ticket2=0;
+	public int ticketThree=0;
+	public int ticket3=0;
+
+	@Override
+	public void settings() // void Awake
+	{
+		size(1440, 1024);
+		img = loadImage("Pantalla Inicio.png");
+	}
+
+	@Override
+	public void setup() // void Start
+	{
+
+	}
+
+	@Override
+	public void draw() // void Update
+	{
+		image(img, 0, 0);
+	}
+
+}
